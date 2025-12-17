f048d68fc66a61a89a8fb110a7c35184087a2b19
bala-amypo <bala@amypo.in>
2025-12-17 06:40:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/studententity.java
+++ src/main/java/com/example/demo/entity/studententity.java
@@ -1,38 +1,42 @@
 package.com.example.demo.entity
-
+import jakarta.persistance.Entity;
+import jakarta.persistance.Id;
+import jakarta.persistance.GeneartedValue;
+import jakarta.persistance.GenerationType;
 @Entity
-
-
 public class studententity{
+    @Id
+    @GeneartedValue(strategy = GenerationType.IDENTITY)
+
     private Integer id;
     private String user;
     private String email;
     private String password;
     private Date date;
 
-public Integer getId() {return id;}
-public void setId(Integer id) {this.id=id;}
+    public Integer getId() {return id;}
+    public void setId(Integer id) {this.id=id;}
 
-public String getUser() {return user;}
-public void setUser(String user) {this.user=user}
+    public String getUser() {return user;}
+    public void setUser(String user) {this.user=user}
 
-public String getEmail() {return email;}
-public void setEmail(String email) {this.email=email}
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email=email}
 
-public String getPassword() {return password;}
-public void setPassword(String password) {this.password=password}
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password=password}
 
-public Date getUser() {return date;}
-public void setUser(Date date) {this.date=date}
+    public Date getUser() {return date;}
+    public void setUser(Date date) {this.date=date}
 
-public studententitty(Integer id, String user,String email,String password,Date date){
+    public studententity(Integer id, String user,String email,String password,Date date){
     this.id=id;
     this.user=user;
     this.email.email;
     this.password=password;
     this.date=date;
-}
-public studententity(){
-
-}
+    }
+    public studententity(){
+        
+    }
 }

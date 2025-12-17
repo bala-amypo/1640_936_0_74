a2d03a7667a484364069443e0221b1a5398b957b
bala-amypo <bala@amypo.in>
2025-12-17 06:25:41 +0000

    User commit

--- src/main/java/com/example/demo/entity/studententity.java
+++ src/main/java/com/example/demo/entity/studententity.java
@@ -1,16 +1,34 @@
-package cam.example.demo.entity
-
+package.com.example.demo.entity
 
 @Entity
+
+
 public class studententity{
     private Integer id;
-    private string user;
-    private string email;
-    private string password;
+    private String user;
+    private String email;
+    private String password;
     private Date date;
 }
-public string getuser{
+public Integer getId() {return id;}
+public void setId(Integer id) {this.id=id;}
 
-public static id(Itnteger ID){
-    
+public String getUser() {return user;}
+public void setUser(String user) {this.user=user}
+
+public String getEmail() {return email;}
+public void setEmail(String email) {this.email=email}
+
+public String getPassword() {return password;}
+public void setPassword(String password) {this.password=password}
+
+public Date getUser() {return date;}
+public void setUser(Date date) {this.date=date}
+
+public void main(Integer id, String user,String email,String password,Date date){
+    this.id=id;
+    this.user=user;
+    this.email.email;
+    this.password=password;
+    this.date=date;
 }

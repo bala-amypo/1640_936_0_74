deef416ed4fc6f9373d6a7243490bbc57b05a85f
bala-amypo <bala@amypo.in>
2025-12-17 06:55:41 +0000

    User commit

--- src/main/java/com/example/demo/entity/studententity.java
+++ src/main/java/com/example/demo/entity/studententity.java
@@ -15,25 +15,45 @@ public class studententity{
     private String password;
     private Date date;
 
-    public Integer getId() {return id;}
-    public void setId(Integer id) {this.id=id;}
+    public Integer getId() {
+        return id;
+        }
+    public void setId(Integer id) {
+        this.id=id;
+        }
 
-    public String getUser() {return user;}
-    public void setUser(String user) {this.user=user}
+    public String getUser() {
+        return user;
+    }
+    public void setUser(String user) {
+        this.user=user;
+        }
 
-    public String getEmail() {return email;}
-    public void setEmail(String email) {this.email=email}
+    public String getEmail() {
+        return email;
+        }
+    public void setEmail(String email) {
+        this.email=email;
+        }
 
-    public String getPassword() {return password;}
-    public void setPassword(String password) {this.password=password}
+    public String getPassword() {
+        return password;
+        }
+    public void setPassword(String password) {
+        this.password=password;
+        }
 
-    public Date getUser() {return date;}
-    public void setUser(Date date) {this.date=date}
+    public Date getUser() {
+        return date;
+        }
+    public void setUser(Date date) {
+        this.date=date
+        }
 
     public studententity(Integer id, String user,String email,String password,Date date){
     this.id=id;
     this.user=user;
-    this.email.email;
+    this.email=email;
     this.password=password;
     this.date=date;
     }

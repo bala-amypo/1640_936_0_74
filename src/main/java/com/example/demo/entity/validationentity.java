a78daf99e072f7f97521759d384ab11813c9c455
bala-amypo <bala@amypo.in>
2025-12-18 08:29:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/validationentity.java
+++ src/main/java/com/example/demo/entity/validationentity.java
@@ -1,14 +1,15 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Positive;
+
 @Entity
 public class validationentity{
     @Id
@@ -17,14 +18,56 @@ public class validationentity{
     @NotNull
     @Size(min=2,max=10,message="must be 2 to 10 character")
     private String username;
-    @email(message="Email is not valid")
+    @Email(message = "Email is not valid")
     private String email;
-    @Size(min=2,max=10)
-    @NotNull(message="Password is mandatory")
+    @Size(min=2,max=10,message="must be 2 to 10 character")
+    @NotNull(message = "Password is mandatory")
     private String password;
     @Max(30)
     @Positive(message="Age must be a positive number")
     private int age;
+    public Long getId(){
+        return id;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public void setUsername(String username){
+        this.username=username;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+    public validationentity(Long id,
+    @NotNull @Size(min=2,max=10,message="must be 2 to 10 character") String username,
+    @Email(message = "Email is not valid") String email,
+    @Size(min=2,max=10,message="must be 2 to 10 character") @NotNull(message = "Password is mandatory")String password,
+    @Max(30) @Positive(message="Age must be a positive number") int age){
+        this.id=id;
+        this.username = username;
+        this.password=password;
+        this.email=email;
+        this.age=age;
+    }
+    )
 
-
+    
 }

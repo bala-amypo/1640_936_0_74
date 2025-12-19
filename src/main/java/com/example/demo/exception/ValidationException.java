4f1ca735e65b0b73257d4858655328df1fa49b7d
bala-amypo <bala@amypo.in>
2025-12-19 04:30:52 +0000

    User commit

--- src/main/java/com/example/demo/exception/ValidationException.java
+++ src/main/java/com/example/demo/exception/ValidationException.java
@@ -1,6 +1,9 @@
 package com.example.demo.exception;
-public class ValidationException extends RuntimeException(String message){
+public class ValidationException extends RuntimeException{
+    public ValidationException(String message){
+
+    
     super(message);
     
-
+}
 }

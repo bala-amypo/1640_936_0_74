dc0da3668089532e79c582f630d0fa902fac4216
bala-amypo <bala@amypo.in>
2025-12-19 04:42:52 +0000

    User commit

--- src/main/java/com/example/demo/service/ValidationService.java
+++ src/main/java/com/example/demo/service/ValidationService.java
@@ -6,7 +6,7 @@ import com.example.demo.entity.ValidationEntity;
 public interface ValidationService{
     ValidationEntity vpostData(ValidationEntity stu);
         //StudentEntity getData(); 
-    // List<StudentEntity>getAllData();
+    List<StudentEntity>get();
     // String DeleteData(int id);
     // StudentEntity getData(int id);
     // StudentEntity updateData(int id,StudentEntity entity);

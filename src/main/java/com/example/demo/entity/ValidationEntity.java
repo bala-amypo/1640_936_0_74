00584a8a72b060febfde257136710136bcfb6ee9
bala-amypo <bala@amypo.in>
2025-12-19 08:00:08 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -9,8 +9,14 @@ import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Positive;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ValidationEntity{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -26,50 +32,6 @@ public class ValidationEntity{
     @Max(30)
     @Positive(message="Age must be a positive number")
     private int age;
-    public Long getId(){
-        return id;
-    }
-    public String getUsername(){
-        return username;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public int getAge(){
-        return age;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-    public void setUsername(String username){
-        this.username=username;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
-    public void setAge(int age){
-        this.age=age;
-    }
-    public ValidationEntity(Long id,
-    @NotNull @Size(min=2,max=10,message="must be 2 to 10 character") String username,
-    @Email(message = "Email is not valid") String email,
-    @Size(min=2,max=10,message="must be 2 to 10 character") @NotNull(message = "Password is mandatory")String password,
-    @Max(30) @Positive(message="Age must be a positive number") int age){
-        this.id=id;
-        this.username = username;
-        this.password=password;
-        this.email=email;
-        this.age=age;
-    }
-    public ValidationEntity(){
-        
-    }
 
     
 }
